6e8ab72e2cec1898c926351668781492df963344
Brian Anthony Jacobe <brian.a.jacobe@gmail.com>
2023-01-02 14:00:19 -0800

    Adding TOP practice js files

---
+++ javascript-practice/main.js
@@ -0,0 +1,30 @@
+/**
+ * main.js
+ * Foundation Fundamentals Part 1
+ */
+
+console.log('--------------------------------');
+console.log('Foundation Fundamentals Part 1');
+console.log('--------------------------------');
+console.log(23+97);
+
+let sumTotal = 0;
+let myValues = [1, 2, 3, 4, 5, 6];
+
+for (let i = 0; i < myValues.length; i++) {
+    sumTotal += myValues[i];
+}
+console.log(sumTotal);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+console.log(9 * a);
+let b = 7 * a;
+console.log(b);
+
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+console.log(percentage);

81259a0644fe021c1a0f1568dafbd87c6db10df8
Brian Anthony Jacobe <brian.a.jacobe@gmail.com>
2023-01-02 17:47:13 -0800

    Added more JS exercises/practice solutions

--- javascript-practice/main.js
+++ javascript-practice/main.js
@@ -29,11 +29,39 @@ let actual = MAX - 13;
 let percentage = actual / MAX;
 console.log(percentage);
 
-const button = document.querySelector("button");
+// const button = document.querySelector("button");
 
-let greeting = () => {
-    const name = prompt("What is your name?");
-    alert(`Hello ${name}, nice to meet you!`);
-}
+// let greeting = () => {
+//     const name = prompt("What is your name?");
+//     alert(`Hello ${name}, nice to meet you!`);
+// }
 
-button.addEventListener("click", greeting);
+// button.addEventListener("click", greeting);
+
+console.log('--------------------------------');
+console.log('Foundation Fundamentals Part 2');
+console.log('--------------------------------');
+// String Methods
+let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+let message = 'I\'m joining Google!';
+console.log(alphabet.length);
+console.log(alphabet.slice(2,alphabet.length));
+console.log(message.replace("Google", "Microsoft"));
+console.log(message.toUpperCase());
+let userAge = 31;
+let checkAge = (userAge) => {
+    let ageLimit = 35;
+    let ageResponse = '';
+    if (userAge > ageLimit) {
+        ageResponse = "You are too old!";
+    } else if (userAge === ageLimit) {
+        ageResponse = "Let's purchase that condo!";
+    }
+    else {
+        ageResponse = "You are not too old!";
+    }
+    return ageResponse;
+};
+
+console.log(checkAge(userAge));
+console.log(checkAge(35));

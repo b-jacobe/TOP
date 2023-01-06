e9c5478cdf3d2e6810600aaa608ae957069e7b81
Brian Anthony Jacobe <brian.a.jacobe@gmail.com>
2023-01-05 20:13:31 -0800

    Adding TOP JavaScript exercise file

--- javascript-practice/main.js
+++ javascript-practice/main.js
@@ -96,3 +96,21 @@ const textBox = document.querySelector("#textBox");
 const output = document.querySelector("#output");
 
 textBox.addEventListener('keydown', (event) => output.textContent = `You pressed "${event.key}".`);
+
+console.log('--------------------------------');
+console.log('Foundations Problem Solving');
+console.log('--------------------------------');
+
+//FizzBuzz
+let input = parseInt(prompt('Please enter a number to determine FizzBuzz'));
+for (let i = 0; i < input; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log('FizzBuzz');
+    } else if (i % 3 === 0){
+        console.log('Fizz');
+    } else if (i % 5 === 0){
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
+}
